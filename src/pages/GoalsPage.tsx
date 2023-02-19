10b4b9277152ca897667ee51f964844b8841f2ab
Kasparas <kasparas.baniulis@gmail.com>
2023-02-19 10:10:52 +0200

    Added goals list items

--- src/pages/GoalsPage.tsx
+++ src/pages/GoalsPage.tsx
@@ -1,13 +1,59 @@
+import { useState } from "react"
 import { ReactComponent as FilterIcon } from "../assets/filter.svg"
 import { ReactComponent as PlusIcon } from "../assets/plus.svg"
 import { ReactComponent as ArrowIcon } from "../assets/arrow.svg"
+import { ReactComponent as TimerStartIcon } from "../assets/timer_start.svg"
+
+interface Task {
+  title: string
+}
+
+interface Goal {
+  title: string
+  tasks: Task[]
+}
+
+const goals: Goal[] = [
+  {
+    title: "Make a todo app",
+    tasks: [
+      {
+        title: "Make a Figma sketch",
+      },
+      {
+        title: "Learn Next.js",
+      },
+      {
+        title: "Make a timer app",
+      },
+    ],
+  },
+  {
+    title: "Learn to play chess",
+    tasks: [
+      {
+        title: "Learn chess rules",
+      },
+      {
+        title: "Learn opening moves",
+      },
+      {
+        title: "Play a match with dad",
+      },
+    ],
+  },
+  {
+    title: 'Finish "The Shape of Space"',
+    tasks: [],
+  },
+]
 
 function GoalsPage() {
   return (
     <div className="mt-4 flex justify-between max-[768px]:px-6 md:justify-center md:space-x-36">
       <div className="w-[650px]">
         <GoalsHeader />
-        <GoalsList />
+        <GoalsList goals={goals} />
       </div>
     </div>
   )
@@ -28,24 +74,62 @@ function GoalsHeader() {
   )
 }
 
-function GoalsList() {
+function GoalsList({ goals }: { goals: Goal[] }) {
   return (
     <ul className="space-y-3">
-      <GoalItem />
-      <GoalItem />
-      <GoalItem />
+      {goals.map(goal => (
+        <GoalItem goal={goal} />
+      ))}
     </ul>
   )
 }
 
-function GoalItem() {
+function GoalItem({ goal }: { goal: Goal }) {
+  const [showSublist, setShowSublist] = useState(true)
+  const containsSublist = !!goal.tasks.length
+
   return (
     <li>
-      <div className="flex cursor-pointer rounded-full bg-red-400 py-3 px-6">
-        <div>Finish "The Shape of Space"</div>
-        <ArrowIcon className="ml-auto" />
+      <div
+        onClick={() => setShowSublist(prev => !prev)}
+        className="flex space-x-3"
+      >
+        <div className="flex w-full cursor-pointer rounded-full bg-red-400 py-3 px-6">
+          <div>{goal.title}</div>
+          {containsSublist && (
+            <ArrowIcon
+              className={`ml-auto ${showSublist ? "rotate-180" : ""}`}
+            />
+          )}
+        </div>
+        {!containsSublist && (
+          <div className="my-auto aspect-square w-12 cursor-pointer rounded-full bg-red-400">
+            <TimerStartIcon className="m-auto h-full" />
+          </div>
+        )}
       </div>
+      {showSublist && <GoalSublist tasks={goal.tasks} />}
     </li>
+  )
+}
+
+function GoalSublist({ tasks }: { tasks: Task[] }) {
+  return (
+    <div>
+      <ul className="mt-3 ml-6 space-y-3">
+        {tasks.map(task => (
+          <li className="flex space-x-3">
+            <div className="my-auto aspect-square w-4 rounded-full bg-gray-500"></div>
+            <div className="flex w-full cursor-pointer rounded-full bg-red-400 py-3 px-6">
+              <div>{task.title}</div>
+            </div>
+            <div className="my-auto aspect-square w-12 cursor-pointer rounded-full bg-red-400">
+              <TimerStartIcon className="m-auto h-full" />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
