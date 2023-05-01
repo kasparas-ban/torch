82018a3d39c24de0e4e7c0ff484717f81d52af99
Kasparas <kasparas.baniulis@gmail.com>
2023-05-01 11:48:20 +0300

    Updated duration input style

--- src/components/Modals/Inputs.tsx
+++ src/components/Modals/Inputs.tsx
@@ -315,11 +315,11 @@ function TimerInput({
 }) {
   return (
     <div className="w-42">
-      <div className="relative mt-1 flex w-full flex-row gap-3 bg-transparent max-[400px]:flex-wrap max-[400px]:gap-2">
-        <div className="flex h-8 flex-grow flex-row max-[400px]:basis-full">
+      <div className="relative mt-1 flex w-full flex-row gap-3 bg-transparent max-[420px]:flex-wrap max-[420px]:gap-2">
+        <div className="flex h-9 flex-grow flex-row max-[420px]:basis-full">
           <button
             data-action="decrement"
-            className="h-full cursor-pointer rounded-l-xl bg-gray-400 px-1 text-gray-600 outline-none drop-shadow-lg hover:bg-gray-500 hover:text-gray-700"
+            className="h-full cursor-pointer rounded-l-xl bg-gray-300 px-2.5 text-gray-500 outline-none drop-shadow-lg hover:bg-gray-400 hover:text-gray-100"
             onClick={e => {
               e.preventDefault()
               setDuration(
@@ -331,12 +331,12 @@ function TimerInput({
             }}
           >
             <span className="m-auto flex justify-center text-2xl font-thin">
-              <MinusIcon />
+              <MinusIcon className="h-6 w-6 stroke-[2.5]" />
             </span>
           </button>
           <input
             type="number"
-            className="text-md md:text-basecursor-default flex w-full items-center bg-gray-200 text-center font-semibold  text-gray-700 outline-none hover:text-black focus:text-black  focus:outline-none"
+            className="text-md md:text-basecursor-default flex w-full items-center bg-gray-200 text-center text-2xl font-semibold  text-gray-700 outline-none hover:text-black focus:text-black  focus:outline-none"
             name="timer"
             min={0}
             max={99}
@@ -347,7 +347,7 @@ function TimerInput({
           ></input>
           <button
             data-action="increment"
-            className="h-full cursor-pointer rounded-r-xl bg-gray-400 px-1 text-gray-600 drop-shadow-lg hover:bg-gray-500 hover:text-gray-700"
+            className="h-full cursor-pointer rounded-r-xl bg-gray-300 px-2 text-gray-500 drop-shadow-lg hover:bg-gray-400 hover:text-gray-100"
             onClick={e => {
               e.preventDefault()
               setDuration(
@@ -356,35 +356,35 @@ function TimerInput({
               )
             }}
           >
-            <span className="m-auto flex justify-center text-2xl font-thin">
-              <PlusIcon />
+            <span className="m-auto flex justify-center px-0.5 text-2xl font-thin">
+              <PlusIcon className="h-6 w-6 stroke-[2.5]" />
             </span>
           </button>
-          <span className="my-auto ml-2.5 h-full text-lg font-medium text-gray-500 max-[400px]:basis-4/12">
+          <span className="my-auto ml-2.5 flex h-full items-center text-lg font-medium text-gray-500 max-[420px]:basis-4/12">
             h
           </span>
         </div>
-        <div className="flex h-8 flex-grow flex-row">
+        <div className="flex h-9 flex-grow flex-row">
           <button
             data-action="decrement"
-            className="h-full cursor-pointer rounded-l-xl bg-gray-400 px-1 text-gray-600 outline-none drop-shadow-lg hover:bg-gray-500 hover:text-gray-700"
+            className="h-full cursor-pointer rounded-l-xl bg-gray-300 px-1 text-gray-500 drop-shadow-md hover:bg-gray-400 hover:text-gray-100"
             onClick={e => {
               e.preventDefault()
               setDuration(
                 duration?.hours?.toString() ?? "",
                 duration?.minutes && duration.minutes > 0
-                  ? (Number(duration.minutes) - 1).toString()
+                  ? Math.max(Number(duration.minutes) - 5, 0).toString()
                   : "0"
               )
             }}
           >
-            <span className="m-auto flex justify-center text-2xl font-thin">
-              <MinusIcon />
+            <span className="m-auto flex items-center justify-center px-2 text-xl font-semibold">
+              -5
             </span>
           </button>
           <input
             type="number"
-            className="text-md md:text-basecursor-default flex w-full items-center bg-gray-200 text-center font-semibold  text-gray-700 outline-none hover:text-black focus:text-black  focus:outline-none"
+            className="text-md md:text-basecursor-default flex w-full items-center bg-gray-200 text-center text-2xl font-semibold  text-gray-700 outline-none hover:text-black focus:text-black  focus:outline-none"
             name="timer"
             min={0}
             max={99}
@@ -395,22 +395,22 @@ function TimerInput({
           ></input>
           <button
             data-action="increment"
-            className="h-full cursor-pointer rounded-r-xl bg-gray-400 px-1 text-gray-600 drop-shadow-lg hover:bg-gray-500 hover:text-gray-700"
+            className="h-full cursor-pointer rounded-r-xl bg-gray-300 px-2 text-gray-500 drop-shadow-md hover:bg-gray-400 hover:text-gray-100"
             onClick={e => {
               e.preventDefault()
               setDuration(
                 duration?.hours?.toString() ?? "",
-                duration?.minutes
-                  ? (Number(duration.minutes) + 1).toString()
-                  : "1"
+                !isNaN(Number(duration?.minutes))
+                  ? Math.min(Number(duration?.minutes) + 5, 60).toString()
+                  : "0"
               )
             }}
           >
-            <span className="m-auto flex justify-center text-2xl font-thin">
-              <PlusIcon />
+            <span className="m-auto flex items-center justify-center pr-1 text-xl font-semibold">
+              +5
             </span>
           </button>
-          <span className="my-auto ml-2.5 h-full text-lg font-medium text-gray-500 max-[400px]:basis-4/12">
+          <span className="my-auto ml-2.5 flex h-full items-center text-lg font-medium text-gray-500 max-[420px]:basis-4/12">
             min
           </span>
         </div>
