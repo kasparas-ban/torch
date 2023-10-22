3fcec024bd8855e859ad92a8651a4f4ed13d8aed
Kasparas <kasparas.baniulis@gmail.com>
2023-10-22 18:16:10 +0300

    Env var test

--- src/components/DevMarker/DevMarker.tsx
+++ src/components/DevMarker/DevMarker.tsx
@@ -2,6 +2,8 @@ export default function DevMarker() {
   const isDev =
     import.meta.env.MODE === "development" || import.meta.env.VITE_DEV_VERCEL
 
+  console.log("Is dev: ", isDev, import.meta.env.VITE_DEV_VERCEL)
+
   return isDev ? (
     <div className="absolute right-2 top-2 rounded-md bg-rose-600 px-3 py-1 font-bold tracking-widest text-white sm:bottom-4 sm:right-4 sm:top-auto">
       DEV
